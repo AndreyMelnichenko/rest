0b32d304d17c4733c39e7cb6f32a1663eac57509
Andrii Melnychenko <melnichenko.andrey@gmail.com>
2018-04-17 01:37:25 +0300

    save at step 4.1

--- src/main/java/epay/TokenResultFields.java
+++ src/main/java/epay/TokenResultFields.java
@@ -3,9 +3,11 @@ package epay;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TokenResultFields {
     @JsonProperty("AccessToken")
     private String accessToken;
