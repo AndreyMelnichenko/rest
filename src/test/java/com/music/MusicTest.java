5e7b8ee240f6f7cb476ac50f400e5969644300e4
Andrii Melnychenko <melnichenko.andrey@gmail.com>
2018-05-05 08:46:28 +0300

    add swagger connection

--- src/test/java/com/music/MusicTest.java
+++ src/test/java/com/music/MusicTest.java
@@ -6,10 +6,7 @@ import guru.nidi.ramltester.RamlLoaders;
 import guru.nidi.ramltester.restassured3.RestAssuredClient;
 import io.restassured.RestAssured;
 import org.testng.annotations.Test;
-
-import static guru.nidi.ramltester.junit.RamlMatchers.validates;
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
 
 public class MusicTest extends TestBase {
 
@@ -27,5 +24,23 @@ public class MusicTest extends TestBase {
                 .body("name", equalTo("Prodigy"))
                 .when()
                 .get("music");
+
+
+    }
+
+    @Test
+    public void car(){
+        RestAssured.baseURI="http://localhost:8080";
+
+        RamlDefinition api = RamlLoaders.fromClasspath().load("api2.raml");
+        //       assertThat(api.validate(), validates());
+
+        RestAssuredClient assuredClient = api.failFast().createRestAssured3();
+
+        assuredClient.given().queryParam("id","1")
+                .expect()
+                .body("brand", equalTo("BMW"))
+                .when()
+                .get("car");
     }
 }
